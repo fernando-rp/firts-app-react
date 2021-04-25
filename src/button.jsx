c6393c812f4268569ba75eac704d0b1ac35131d5
Fernando Rios <fernandorios@MacBook-Pro-de-Fernando.local>
2021-04-25 19:44:48 -0400

    firts commit

---
+++ src/button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export function Button(){
+
+    return <button>Click on me</button>
+}
---
+++ src/saludo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Button} from './button.jsx';
+
+
+function Saludo(){
+    return ( 
+        <>
+        <h1>Hola Mundo 4Geeks</h1>
+        <Button/>
+        </>
+    )
+}
+
+export default Saludo;
